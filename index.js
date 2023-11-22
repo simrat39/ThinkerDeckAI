890075fa21cb6b802308932c76c0ce6d494c1242
Gurtej Malik <gurtejmalik@gmail.com>
2023-11-22 13:38:14 -0800

    Created routing syntax and basic error page

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
+require("./utils.js");
+const express = require("express"); // imports the express.js module and assigns it to a constant variable named express
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-    res.send("Hi from express")
-})
+app.get("/", (req, res) => {
+    res.render("homepage", {});
+});
 
 const port = 8000
 app.listen(port, () => {
     console.log("Running on port " + port)
 })
+
+// 404 error
+app.get("*", (req, res) => {
+    res.status(404);
+    res.render("404");
+});
