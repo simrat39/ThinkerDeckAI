eb5ff0ab2208b89431379e9786a692847801730a
Simrat Grewal <simrats169169@gmail.com>
2023-11-22 10:54:57 -0800

    Add basic express boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+const express = require('express')
+const app = express()
+
+app.get("/", function(req, res) {
+    res.send("Hi from express")
+})
+
+const port = 8000
+app.listen(port, () => {
+    console.log("Running on port " + port)
+})
