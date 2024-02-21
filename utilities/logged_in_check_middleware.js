b56e5c2ed639781f8123c9f3becbf29502b8ddf1
Gurtej Malik <gurtejmalik@gmail.com>
2024-02-21 01:06:59 -0800

    Add documentation

--- utilities/logged_in_check_middleware.js
+++ utilities/logged_in_check_middleware.js
@@ -1,3 +1,9 @@
+/**
+ * Middleware function to check if a user is logged in.
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @param {Function} next - The next middleware function.
+ */
 export default function logged_in_check(req, res, next) {
     console.log(req.user)
     if (req.user) {
